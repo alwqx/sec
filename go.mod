8c5d3bdfd839c9135ccb93b18bbb64f074379183
alwqx <kenan3015@gmail.com>
2024-09-30 16:03:49 +0800

    fix charset

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/alwqx/sec
 go 1.22.5
 
 require (
-	github.com/carlmjohnson/requests v0.24.2 // indirect
+	github.com/spf13/cobra v1.8.1
+	golang.org/x/text v0.18.0
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/net v0.27.0 // indirect
 )
--- provider/sina.go
+++ provider/sina.go
@@ -1,33 +1,44 @@
 package provider
 
 import (
-	"context"
 	"fmt"
+	"io"
 	"log/slog"
+	"net/http"
+	"strings"
 
-	"github.com/carlmjohnson/requests"
+	"golang.org/x/text/encoding/simplifiedchinese"
 )
 
 type SinaProvider struct{}
 
 func Search(key string) {
 	var (
-		res string
 		err error
 	)
 	reqUrl := fmt.Sprintf("https://suggest3.sinajs.cn/suggest/type=11,12,15,21,22,23,24,25,26,31,33,41&key=%s", key)
-	err = requests.
-		URL(reqUrl).
-		ContentType("application/json;charset=UTF-8").
-		Accept("application/json, text/javascript, */*; q=0.01").
-		Header("Accept-Language", "zh-CN,zh;q=0.9").
-		Header("Origin", "https://yield.chinabond.com.cn").
-		Header("Host", "yield.chinabond.com.cn").
-		ToString(&res).
-		Fetch(context.Background())
+
+	resp, err := http.DefaultClient.Get(reqUrl)
 	if err != nil {
-		slog.Error("[RequestChinaBond] request %s error: %v", reqUrl, err)
+		slog.Error("[Search] request %s error: %v", reqUrl, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	var resBytes []byte
+	resBytes, err = io.ReadAll(resp.Body)
+	if err != nil {
+		slog.Error("[Search] request %s error: %v", reqUrl, err)
+		return
 	}
 
-	fmt.Println(res)
+	if strings.Contains(resp.Header.Get("Content-Type"), "charset=GBK") {
+		resBytes, err = simplifiedchinese.GBK.NewDecoder().Bytes(resBytes)
+	}
+	if err != nil {
+		slog.Error("[Search] request %s error: %v", reqUrl, err)
+		return
+	}
+
+	fmt.Println(string(resBytes))
 }
