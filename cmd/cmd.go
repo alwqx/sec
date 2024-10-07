c69c9139e5e359058047aefdce77a44463b475b6
alwqx <kenan3015@gmail.com>
2024-10-07 09:56:07 +0800

    info remove title

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -103,7 +103,8 @@ func InfoHandler(cmd *cobra.Command, args []string) error {
 	opts.Code = sec.Code
 	opts.ExCode = sec.ExCode
 	profile := sina.Profile(opts)
-	fmt.Printf("基本信息\n证券代码\t%s\n简称历史\t%s\n公司名称\t%s\n上市日期\t%s\n发行价格\t%.2f\n行业分类\t%s\n主营业务\t%s\n办公地址\t%s\n公司网址\t%s\n当前价格\t%.2f\n市净率PB\t%.2f\n市盈率TTM\t%.2f\n总市值  \t%s\n流通市值\t%s\n",
+	profile.ExCode = sec.ExChange
+	fmt.Printf("证券代码\t%s\n简称历史\t%s\n公司名称\t%s\n上市日期\t%s\n发行价格\t%.2f\n行业分类\t%s\n主营业务\t%s\n办公地址\t%s\n公司网址\t%s\n当前价格\t%.2f\n市净率PB\t%.2f\n市盈率TTM\t%.2f\n总市值  \t%s\n流通市值\t%s\n",
 		sec.ExCode, profile.HistoryName, profile.Name, profile.ListingDate, profile.ListingPrice,
 		profile.Category, profile.MainBusiness, profile.BusinessAddress, profile.WebSite,
 		profile.Current, profile.PB, profile.PeTTM, humanNum(profile.MarketCap), humanNum(profile.TradedMarketCap))
