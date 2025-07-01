1dfb3f77b031696962af9242d58f0300acfb3bbf
alwqx <kenan3015@gmail.com>
2025-07-01 23:23:21 +0800

    add eastmoney provider

---
+++ provider/eastmoney/eastmoney_test.go
@@ -0,0 +1,115 @@
+package eastmoney
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/alwqx/sec/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetOriginQuoteHistory(t *testing.T) {
+	t.Skip("仅用于开发调试")
+	req := &GetQuoteHistoryReq{
+		Code:       "600036",
+		MarketCode: 1,
+		Begin:      "20250106",
+		End:        "20250106",
+	}
+	resp, err := getOriginQuoteHistory(req)
+	require.Nil(t, err)
+	err = utils.WriteJson(resp, "./quote_history.json")
+	require.Nil(t, err)
+}
+
+func TestParseKlineItem(t *testing.T) {
+	// 1. empty
+	_, err := ParseKlineItem("")
+	require.Equal(t, ErrInvalidKLine, err)
+
+	// 2. common
+	line := "2024-12-26,39.40,39.48,39.54,39.01,539252,2125139425.00,1.35,0.20,0.08,0.26"
+	res, err := ParseKlineItem(line)
+	require.Nil(t, err)
+	require.Equal(t, "2024-12-26", res.Date.Format(utils.LayoutYYMMDD))
+	require.EqualValues(t, 39.40, res.Open)
+	require.EqualValues(t, 39.48, res.Close)
+	require.EqualValues(t, 39.54, res.High)
+	require.EqualValues(t, 39.01, res.Low)
+	require.EqualValues(t, 539252, res.Volume)
+	require.EqualValues(t, 2125139425.00, res.TurnOver)
+	require.EqualValues(t, 1.35, res.Amplitude)
+	require.EqualValues(t, 0.08, res.Change)
+	require.EqualValues(t, 0.20, res.ChangeRate)
+	require.EqualValues(t, 0.26, res.Velocity)
+}
+
+func TestParseQuoteHistoryResp(t *testing.T) {
+	res, err := ParseQuoteHistoryResp(nil)
+	require.NotNil(t, err)
+	require.Nil(t, res)
+
+	rawJson := `{
+    "rc": 0,
+    "rt": 17,
+    "svr": 177617937,
+    "lt": 1,
+    "full": 0,
+    "dlmkts": "",
+    "data": {
+        "code": "600036",
+        "market": 1,
+        "name": "招商银行",
+        "decimal": 2,
+        "dktotal": 5447,
+        "preKPrice": 38.35,
+        "prePrice": 39.34,
+        "qtMiscType": 3,
+        "version": 0,
+        "klines": [
+            "2024-12-24,38.35,39.22,39.29,38.32,993428,3875223368.00,2.53,2.27,0.87,0.48",
+            "2024-12-25,39.23,39.40,39.56,38.99,755913,2972981821.00,1.45,0.46,0.18,0.37",
+            "2024-12-26,39.40,39.48,39.54,39.01,539252,2125139425.00,1.35,0.20,0.08,0.26"
+        ]
+    }
+}`
+	var resp QuoteHistoryResp
+	err = json.Unmarshal([]byte(rawJson), &resp)
+	require.Nil(t, err)
+	res, err = ParseQuoteHistoryResp(&resp)
+	require.Nil(t, err)
+	require.NotNil(t, res)
+	require.EqualValues(t, 3, len(res))
+	require.EqualValues(t, 38.35, res[0].Open)
+	require.EqualValues(t, 39.40, res[1].Close)
+	require.EqualValues(t, 539252, res[2].Volume)
+	require.EqualValues(t, 0.26, res[2].Velocity)
+
+	rawJsonEmptyKline := `{
+  "rc": 0,
+  "rt": 17,
+  "svr": 2887161714,
+  "lt": 1,
+  "full": 0,
+  "dlmkts": "",
+  "data": {
+    "code": "600036",
+    "market": 1,
+    "name": "招商银行",
+    "decimal": 2,
+    "dktotal": 5448,
+    "preKPrice": 0,
+    "prePrice": 39.62,
+    "qtMiscType": 7,
+    "version": 0,
+    "klines": []
+  }
+}`
+	var resp2 QuoteHistoryResp
+	err = json.Unmarshal([]byte(rawJsonEmptyKline), &resp2)
+	require.Nil(t, err)
+	res, err = ParseQuoteHistoryResp(&resp2)
+	require.Nil(t, err)
+	require.NotNil(t, res)
+	require.EqualValues(t, 0, len(res))
+}
---
+++ provider/eastmoney/types.go
@@ -0,0 +1,116 @@
+package eastmoney
+
+import "time"
+
+const (
+	TimeYYMMDD = "20060102"
+)
+
+type CurrentStockInfoDiff struct {
+	F2   float64 `json:"f2"`   // 最新价格
+	F3   float64 `json:"f3"`   // 涨跌幅
+	F4   float64 `json:"f4"`   // 涨跌额
+	F5   float64 `json:"f5"`   // 成交量
+	F6   float64 `json:"f6"`   // 成交额
+	F7   float64 `json:"f7"`   // 振幅
+	F8   float64 `json:"f8"`   // 换手率
+	F9   float64 `json:"f9"`   // 动态市盈率
+	F10  float64 `json:"f10"`  // 量比
+	F12  string  `json:"f12"`  // 证券代码
+	F13  int     `json:"f13"`  // 市场编号 0深证 1上证
+	F14  string  `json:"f14"`  // 证券名称
+	F15  float64 `json:"f15"`  // high
+	F16  float64 `json:"f16"`  // low
+	F17  float64 `json:"f17"`  // open
+	F18  float64 `json:"f18"`  // yclose
+	F20  float64 `json:"f20"`  // 总市值 "-" 表示非股票
+	F21  float64 `json:"f21"`  // 流通市值 "-" 表示非股票
+	F23  float64 `json:"f23"`  // 市净率
+	F37  float64 `json:"f37"`  // 净资产收益率
+	F38  float64 `json:"f38"`  // 总股本
+	F39  float64 `json:"f39"`  // 流通股本
+	F124 int64   `json:"f124"` // 更新时间戳
+	F297 int64   `json:"f297"` // 最新交易日 20241227
+}
+
+type CurrentStockInfoData struct {
+	Total int                      `json:"total"`
+	Diff  []map[string]interface{} `json:"diff"`
+}
+
+type CurrentStockInfoResp struct {
+	Rc     int                   `json:"rc"`
+	Rt     int                   `json:"rt"`
+	Svr    int64                 `json:"svr"`
+	Lt     int                   `json:"lt"`
+	Full   int                   `json:"full"`
+	Dlmkts string                `json:"dlmkts"`
+	Data   *CurrentStockInfoData `json:"data"`
+}
+
+// Quote 基本行情
+type Quote struct {
+	Date       time.Time `json:"date"`        // 交易日期
+	Code       string    `json:"code"`        // 证券代码
+	Name       string    `json:"name"`        // 证券名称
+	Market     int       `json:"market"`      // 证券市场
+	Open       float64   `json:"open"`        // 开盘价
+	Close      float64   `json:"close"`       // 收盘价
+	High       float64   `json:"high"`        // 最高价
+	Low        float64   `json:"low"`         // 最低价
+	Volume     int64     `json:"volume"`      // 成交量
+	TurnOver   float64   `json:"turn_over"`   // 成交额
+	Amplitude  float64   `json:"amplitude"`   // 振幅
+	ChangeRate float64   `json:"change_rate"` // 涨跌幅
+	Change     float64   `json:"change"`      // 涨跌额
+	Velocity   float64   `json:"velocity"`    // TurnOver Rate 换手率
+	Fqt        int       `json:"fqt"`         // 复权类型
+}
+
+type GetQuoteHistoryReq struct {
+	Code       string
+	MarketCode int    // 市场 1 上证，2 深证
+	FQT        int    // 复权类型 0不复权 1前复权 2后复权，默认不复权
+	Begin      string // 开始时间 19000101 格式
+	End        string // 结束时间 20500101 格式
+}
+
+// QuoteHistoryResp 东方财富 K 线历史接口返回数据结构
+type QuoteHistoryResp struct {
+	Rc     int               `json:"rc"`
+	Rt     int               `json:"rt"`
+	Svr    int64             `json:"svr"`
+	Lt     int               `json:"lt"`
+	Full   int               `json:"full"`
+	Dlmkts string            `json:"dlmkts"`
+	Data   *QuoteHistoryData `json:"data"`
+}
+
+// QuoteHistoryData 东方财富 K 线历史接口中 data 字段结构体
+type QuoteHistoryData struct {
+	Code       string   `json:"code"`
+	Market     int      `json:"market"`
+	Name       string   `json:"name"`
+	Decimal    int      `json:"decimal"` // 小数点精确度
+	Dktotal    int      `json:"dktotal"` // 总数据条数
+	PreKPrice  float64  `json:"preKPrice"`
+	PrePrice   float64  `json:"prePrice"`
+	QtMiscType int      `json:"qtMiscType"`
+	Version    int      `json:"version"`
+	Klines     []string `json:"klines"`
+}
+
+// KLineQuote K 线结构体
+type KLineQuote struct {
+	Date       time.Time
+	Open       float64
+	Close      float64
+	High       float64
+	Low        float64
+	Volume     int64   // 成交量
+	TurnOver   float64 // 成交额
+	Amplitude  float64 // 振幅
+	ChangeRate float64 // 涨跌幅
+	Change     float64 // 涨跌额
+	Velocity   float64 // TurnOver Rate 换手率
+}
